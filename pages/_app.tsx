a9de4d6a7f43cefd482f8a25f668c7f6e61660f2
hgpark <digitalworks_hgpark@amorepacific.com>
2023-09-15 13:07:39 +0900

    nextJs setting

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+
+function MyApp() {
+  return (
+    <div></div>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Home;
