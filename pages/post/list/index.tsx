63741b3899fe362b965b7f4478ea1e592c0abe69
hgpark <digitalworks_hgpark@amorepacific.com>
2023-09-27 13:23:31 +0900

    페이지네이션 작업 완료

--- pages/post/list/index.tsx
+++ pages/post/list/index.tsx
@@ -6,7 +6,10 @@ import {
   getDocs,
   getDoc,
   doc,
-  startAfter
+  startAfter,
+  QueryDocumentSnapshot,
+  endBefore,
+  limitToLast,
 } from "firebase/firestore";
 import type { NextPage } from "next";
 import { dbService } from "../../api/firebase";
@@ -26,26 +29,71 @@ const PostList: NextPage = () => {
   const [totalNum, setTotalNum] = useState(0);
   const [totalPageNum, setTotalPageNum] = useState(0);
   const [currentPageNum, setCurrentPageNum] = useState(1);
+  const [prevData, setPrevData] = useState<QueryDocumentSnapshot>();
+  const [lastData, setLastData] = useState<QueryDocumentSnapshot>();
+  const [isPrev, setIsPrev] = useState(false);
+  const [isNext, setIsNext] = useState(false);
+  const [isRefetch, setIsRefetch] = useState(false);
   const getPosts = async () => {
     setPostList([]);
-    const queryList = query(
-      collection(dbService, "free"),
-      limit(10),
-      orderBy("createdAt")
-    );
+    let queryList;
+    if (!isPrev && !isNext) {
+      queryList = query(
+        collection(dbService, "free"),
+        limit(10),
+        orderBy("createdAt")
+      );
+    } else if (isNext) {
+      queryList = query(
+        collection(dbService, "free"),
+        limit(10),
+        orderBy("createdAt"),
+        startAfter(lastData)
+      );
+    } else {
+      queryList = query(
+        collection(dbService, "free"),
+        limitToLast(10),
+        orderBy("createdAt"),
+        endBefore(prevData)
+      );
+    }
     const data = await getDocs(queryList);
+    const dataList: PostData[] = [];
     data.forEach((docs) => {
       const postData = { ...docs.data(), id: docs.id } as PostData;
-      setPostList((state) => [...state, postData]);
+      dataList.push(postData);
     });
+    setPostList(dataList);
+    setPrevData(data.docs[0]);
+    setLastData(data.docs[data.docs.length - 1]);
+    isNext && setIsNext(false);
+    isPrev && setIsPrev(false);
+
     const total = await getDoc(doc(dbService, "meta", "boardCount"));
     setTotalNum(total.data()?.total);
-    setTotalPageNum(Math.ceil(total.data()?.total/10));
+    setTotalPageNum(Math.ceil(total.data()?.total / 10));
+  };
+
+  const getNextPage = () => {
+    if (currentPageNum < totalPageNum) {
+      setIsNext(true);
+      setIsRefetch((state) => !state);
+      setCurrentPageNum((state) => state + 1);
+    }
+  };
+
+  const getPrevPage = () => {
+    if (currentPageNum > 1) {
+      setIsPrev(true);
+      setIsRefetch((state) => !state);
+      setCurrentPageNum((state) => state - 1);
+    }
   };
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [isRefetch]);
 
   return (
     <div>
@@ -62,9 +110,15 @@ const PostList: NextPage = () => {
             ))
           : "게시글이 없습니다..."}
       </ul>
-      <p>현재 페이지 {currentPageNum}/{totalPageNum}</p>
-      <button type="button">prev</button>
-      <button type="button">next</button>
+      <p>
+        현재 페이지 {currentPageNum}/{totalPageNum}
+      </p>
+      <button type="button" onClick={getPrevPage}>
+        prev
+      </button>
+      <button type="button" onClick={getNextPage}>
+        next
+      </button>
     </div>
   );
 };
