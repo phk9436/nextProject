8a9022ce0ca3aa883c59ba1697b572e96ac81a88
hgpark <digitalworks_hgpark@amorepacific.com>
2023-10-11 15:53:56 +0900

    홈으로 가기 공통 컴포넌트 추가

---
+++ components/Gohome.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const Gohome = () => {
+  return (
+    <div>
+      <Link href="/">
+        <a>홈으로 가기</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Gohome;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import Gohome from "../components/Gohome";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Gohome />
       <Component {...pageProps} />
     </Layout>
   );
