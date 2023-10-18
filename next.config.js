5642d729411144ac25191bcde5bda1991d239001
hgpark <digitalworks_hgpark@amorepacific.com>
2023-10-18 09:59:40 +0900

    config assetPrefix 수정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   productionBrowserSourceMaps: false,
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? 'https://phk9436.github.io/bilerplate'
+      : '',
 }
 
 module.exports = nextConfig
