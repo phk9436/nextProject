a121a37c53d2296a9eb380e82ef85878d315ea87
hgpark <digitalworks_hgpark@amorepacific.com>
2023-10-31 10:10:15 +0900

    nprogress 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,32 @@
 import "../styles/globals.css";
+import "../styles/nprogress.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import Gohome from "../components/Gohome";
+import {useEffect} from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    const start = () => {
+      NProgress.start();
+    }
+    const end = () => {
+      NProgress.done();
+    };
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+    };
+
+  }, []);
+
   return (
     <Layout>
       <Gohome />
